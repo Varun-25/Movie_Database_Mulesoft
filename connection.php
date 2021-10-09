c3c9eb12fa3d341d79c6a1c4db7b282e052a4686
Varun-25 <66878481+Varun-25@users.noreply.github.com>
2021-10-09 17:17:27 +0530

    Create connection.php

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "Movie_Database_Mulesoft";
+
+$conn = mysqli_connect($servername,$username,$password,$database);
+
+
+?>
